508854fbed0088f602701d330cb958137d6340ca
Phillip <phillip.lam0@gmail.com>
2023-05-16 18:48:23 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "offWhite": "hsl(0, 0%, 94%)",
+      "white": "hsl(0, 0%, 100%)",
+      "lightGrey": "hsl(0, 0%, 86%)",
+      "smokeyGrey": "hsl(0, 1%, 44%)",
+      "offBlack": "hsl(0, 0%, 8%)",
+      "purple": "hsl(259, 100%, 65%)",
+      "lightRed": "hsl(0, 100%, 67%)"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
