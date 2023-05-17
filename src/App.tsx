427094aed55fe7e2035b94e4d87bae225ad03558
Phillip <phillip.lam0@gmail.com>
2023-05-17 11:12:35 -0400

    feat: adding UI Divider and Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Layout } from "./components/UI/Layout";
 import { AgeInput } from "./components/AgeInput";
+import { Divider } from "./components/UI/Divider";
 import { TimeDisplayGroup } from "./components/TimeDisplayGroup";
 
 function App() {
   return (
     <Layout>
       <AgeInput />
+      <Divider />
       <TimeDisplayGroup />
     </Layout>
   );
