2082d5bd3b5f2a1839f59286bf34c2e236ce16ba
Phillip <phillip.lam0@gmail.com>
2023-05-17 15:47:43 -0400

    feat(TextField): add min/max restrictions on inputs

--- src/components/UI/TextField/TextField.tsx
+++ src/components/UI/TextField/TextField.tsx
@@ -1,19 +1,16 @@
-const ERROR_MESSAGE: string = "This field is required";
-
-function ErrorMessage(): JSX.Element {
-  return <div className="italic text-lightRed">{ERROR_MESSAGE}</div>;
-}
-
 export default function TextField(props: {
   label: string;
   placeholder: string;
+  max: number;
+  min?: number;
   error?: boolean;
+  errorMessage?: string;
 }): JSX.Element {
   return (
     <div className="flex flex-col gap-y-2">
       {/* Input Field Label */}
       <div
-        className={`font-bold text-smokeyGrey uppercase tracking-widest ${
+        className={`text-xs sm:text-sm font-bold text-smokeyGrey uppercase tracking-[3px] ${
           props.error && "text-lightRed"
         }`}
       >
@@ -25,12 +22,16 @@ export default function TextField(props: {
         id={props.label}
         name={props.label}
         type="number"
-        className="w-full max-w-[160px] px-6 py-2 border-[1px] border-lightGrey focus:border-purple active:border-purple outline-none rounded-lg text-[32px] placeholder:text-smokeyGrey"
+        className="w-full max-w-[160px] px-4 sm:px-6 py-3 border-[1px] border-lightGrey focus:border-purple active:border-purple outline-none rounded-lg text-xl sm:text-[32px] placeholder:text-smokeyGrey"
         placeholder={props.placeholder}
+        max={props.max}
+        min={props.min}
       />
 
       {/* Error Validation */}
-      {props.error && <ErrorMessage />}
+      {props.error && (
+        <div className="italic text-lightRed">{props.errorMessage}</div>
+      )}
     </div>
   );
 }
