fe1e97e9fc988c5f9aa715ac0fd7c5f59068aa5e
Phillip <phillip.lam0@gmail.com>
2023-05-18 19:55:55 -0400

    feat(TimeDisplayGroup): add placeholder default values

--- src/components/TimeDisplayGroup/TimeDisplayGroup.tsx
+++ src/components/TimeDisplayGroup/TimeDisplayGroup.tsx
@@ -1,21 +1,24 @@
-function TimeDisplay(props: {
-  numValue: number;
-  textValue: string;
-}): JSX.Element {
-  return (
-    <div className="flex flex-row gap-3 font-extrabold text-left text-[60px] sm:text-[75px] md:text-[100px] italic">
-      <p className="text-purple">{props.numValue}</p>
-      <p className="text-black">{props.textValue}</p>
-    </div>
-  );
-}
+import { useState, useEffect } from "react";
 
 export default function TimeDisplayGroup(): JSX.Element {
+  const [years, setYears] = useState("--");
+  const [months, setMonths] = useState("--");
+  const [days, setDays] = useState("--");
+
+  function TimeDisplay(props: { value: string; unit: string }): JSX.Element {
+    return (
+      <div className="flex flex-row gap-3 font-extrabold text-left text-[60px] sm:text-[75px] md:text-[100px] italic">
+        <p className="text-purple">{props.value}</p>
+        <p className="text-black">{props.unit}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col leading-tight">
-      <TimeDisplay numValue={38} textValue="years" />
-      <TimeDisplay numValue={3} textValue="months" />
-      <TimeDisplay numValue={26} textValue="days" />
+      <TimeDisplay value={years} unit="years" />
+      <TimeDisplay value={months} unit="months" />
+      <TimeDisplay value={days} unit="days" />
     </div>
   );
 }
